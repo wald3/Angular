bb0ceed67c6b718edca3473f973f7d871ba19104
Rys Vladislav <rys.vladislavv@gmail.com>
2020-02-19 02:29:46 +0300

    register validation complete

--- angular/ClientApp/src/app/shared/user.service.ts
+++ angular/ClientApp/src/app/shared/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { NgForm, FormBuilder } from '@angular/forms';
+import { NgForm, FormBuilder, Validators, FormGroup } from '@angular/forms';
 //import { User } from './user.model';
 //import { UserSignUp } from './user.singup.model';
 
@@ -12,18 +12,32 @@ export class UserService {
   constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   formModel = this.fb.group({
-    UserName: [''],
-    Email: [''],
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
     FirstName: [''],
     SecondName: [''],
     LastName: [''],
     Passwords : this.fb.group({
-      Password: [''],
-      ConfirmPassword: ['']
-    }),
-    Roles: []
+      Password: ['', [Validators.required, Validators.minLength(6)]],
+      ConfirmPassword: ['', Validators.required]
+    }, {validator: this.comparePasswords }),
+    Roles: [[], { validator: this.roleChecks }]
   });
 
+  roleChecks(fb: FormGroup) {
+    //let 
+  }
+
+  comparePasswords(fb: FormGroup) {
+    let confirmedPassword = fb.get('ConfirmPassword');
+    if (confirmedPassword.errors == null || 'passwordMismatch' in confirmedPassword.errors ) {
+      if (fb.get('Password').value != confirmedPassword.value)
+        confirmedPassword.setErrors({ passwordMismatch: true });
+       else
+        confirmedPassword.setErrors(null);
+    }
+
+  }
 
   getRoles() {
     return this.http.get(this.url + '/GetRoles');
